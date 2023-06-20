edb8accc85a7b49643f163f2e6f377e823fd2974
Ajit Singh <as.ajitsingh@outlook.com>
2023-06-20 18:40:59 +0530

    feat: fixed request imports

--- src/request/request.module.ts
+++ src/request/request.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { RequestController } from './request.controller';
 import { RequestService } from './request.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import { Request, RequestSchema } from './Request.schema';
+import { Request, RequestSchema } from './request.schema';
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: Request.name, schema: RequestSchema }])],
